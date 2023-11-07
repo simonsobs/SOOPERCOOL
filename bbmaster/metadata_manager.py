c5d52083e09ef0f29b3236888ebb6f6c5137b9d4
Adrien La Posta <adrien.la.posta@gmail.com>
2023-11-07 09:25:55 +0000

    load all path to directories as attribute

--- bbmaster/metadata_manager.py
+++ bbmaster/metadata_manager.py
@@ -37,6 +37,9 @@ class BBmeta(object):
         # Basic sanity checks
         if self.lmax > 3*self.nside-1:
             raise ValueError(f"lmax should be lower or equal to 3*nside-1 = {3*self.nside-1}")
+        
+        # Set all the `_directory` attributes
+        self._set_directory_attributes()
 
         # A list of the maps used in the analysis
         self.map_sets_list = self._get_map_sets_list()
@@ -53,6 +56,20 @@ class BBmeta(object):
                 f"{mask_type}_mask_name", 
                 getattr(self, f"_get_{mask_type}_mask_name")()
             )
+
+
+    def _set_directory_attributes(self):
+        """
+        Set the directory attributes that are listed
+        in the paramfiles
+        """
+        for value in self.config.values():
+            # Skip if value is not a dict (e.g. lmax, nside, ...)
+            if not isinstance(value, dict): continue
+            # Loop over 2nd layer dict to find directory definitions
+            for subkey, subvalue in value.items():
+                if subkey.endswith("_directory"):
+                    setattr(self, subkey, subvalue)
 
 
     def _get_map_sets_list(self):
@@ -84,28 +101,28 @@ class BBmeta(object):
         Get the name of the galactic mask.
         """
         fname = f"{self.masks['galactic_mask_root']}_{self.masks['gal_mask_mode']}.fits"
-        return os.path.join(self.masks["mask_directory"], fname)
+        return os.path.join(self.mask_directory, fname)
 
 
     def _get_binary_mask_name(self):
         """
         Get the name of the binary or survey mask.
         """
-        return os.path.join(self.masks["mask_directory"], self.masks["binary_mask"])
+        return os.path.join(self.mask_directory, self.masks["binary_mask"])
 
 
     def _get_point_source_mask_name(self):
         """
         Get the name of the point source mask.
         """
-        return os.path.join(self.masks["mask_directory"], self.masks["point_source_mask"])
+        return os.path.join(self.mask_directory, self.masks["point_source_mask"])
 
 
     def _get_analysis_mask_name(self):
         """
         Get the name of the final analysis mask.
         """
-        return os.path.join(self.masks["mask_directory"], self.masks["analysis_mask"])
+        return os.path.join(self.mask_directory, self.masks["analysis_mask"])
 
 
     def read_mask(self, mask_type):
@@ -145,7 +162,7 @@ class BBmeta(object):
         map_set_root = self.file_root_from_map_set(map_set)
         if id_sim is not None:
             path_to_maps = os.path.join(
-                self.sim_pars["sims_directory"],
+                self.sims_directory,
                 f"{id_sim:04d}"
             )
             os.makedirs(path_to_maps, exist_ok=True)
