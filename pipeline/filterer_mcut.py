79cceff4df03ca8cc33ffd150cd7a318ac9443ea
Bai-Chiang <contact@bai-chiang.com>
2023-11-09 15:44:53 -0500

    Refactor
    Use --args to get directories instead of calling multiple functions,
    which is very hard to read.

--- pipeline/filterer_mcut.py
+++ pipeline/filterer_mcut.py
@@ -1,4 +1,4 @@
-import argparse
+import argparse, glob, os
 import healpy as hp
 from bbmaster.utils import PipelineManager
 import numpy as np
@@ -27,28 +27,37 @@ def filter_map(man, mpQ, mpU, msk, m_cut):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Simple m-cut filter')
+    parser = argparse.ArgumentParser(description='Simple m-cut filter, set all alm with m<=m_cut to zero.')
     parser.add_argument("--globals", type=str,
                         help='Path to yaml with global parameters')
-    parser.add_argument("--first-sim", type=int, help='Index of first sim')
-    parser.add_argument("--num-sims", type=int, help='Number of sims')
-    parser.add_argument("--sim-sorter", type=str,
-                        help='Name of sorting routine')
-    parser.add_argument("--m-cut", type=int, help='m-mode cut')
     parser.add_argument("--output-dir", type=str, help='Output directory')
+
+    parser.add_argument("--sim-dir", type=str, help='Directory contains all simulations (fits format).')
+    parser.add_argument("--filtered-dir", type=str, help='Directory to put all filtered maps.')
+    parser.add_argument("--binary-mask-file", type=str, help='Path to binary mask file.')
+    parser.add_argument("--m-cut", type=int, help='m-mode cut. This will set all alm with m<=m_cut to zero.')
     parser.add_argument("--plot", action='store_true',
                         help='Show last filtered and unfiltered maps?')
     o = parser.parse_args()
     man = PipelineManager(o.globals)
 
-    sorter = getattr(man, o.sim_sorter)
-    file_input_list = sorter(o.first_sim, o.num_sims, o.output_dir,
-                             which='input')
-    file_output_list = sorter(o.first_sim, o.num_sims, o.output_dir,
-                              which='filtered')
+    sim_dir = o.sim_dir
+    if os.path.isabs(o.filtered_dir):
+        filtered_dir = o.filtered_dir
+    else:
+        filtered_dir = os.path.join(o.output_dir, '..', o.filtered_dir)
+    # Create filtered_dir if not exists
+    os.makedirs(filtered_dir, exist_ok=True)
+    if os.path.samefile(o.sim_dir, filtered_dir):
+        raise InputValueError('--sim-dir and --filtered-dir can not be the same directory.')
 
-    msk = hp.read_map(man.fname_binary_mask)
-    for fin, fout in zip(file_input_list, file_output_list):
+    file_input_list = glob.glob(os.path.join(o.sim_dir, '*.fits'))
+    msk = hp.read_map(o.binary_mask_file)
+    
+    for fin in file_input_list:
+        # get basename
+        basename = os.path.basename(fin)
+        fout = os.path.join(filtered_dir, basename)
         # Read maps
         mpQ, mpU = hp.read_map(fin, field=[0, 1])
         # Filter
