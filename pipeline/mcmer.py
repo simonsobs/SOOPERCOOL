eefa748164d40968be4b0ec336100574587df1f8
Adrien La Posta <adrien.la.posta@gmail.com>
2023-11-16 12:05:13 +0000

    adapt mcm computation to new pipeline

--- pipeline/mcmer.py
+++ pipeline/mcmer.py
@@ -4,46 +4,58 @@ import pymaster as nmt
 import healpy as hp
 import os
 from bbmaster.utils import PipelineManager
+from bbmaster import BBmeta
 
 
-def mcmer(o):
-    man = PipelineManager(o.globals)
+def mcmer(args):
+    """
+    Compute the mode coupling matrix
+    from the masks defined in the global
+    parameter file.
 
-    # Number of ells
-    nl = 3*man.nside
-    # Number of polarisation combinations
-    nspec = 4
+    Parameters
+    ----------
+    args : argparse.Namespace
+        Arguments from the command line.
+    """
+    meta = BBmeta(args.globals)
+
+    coupling_dir = meta.coupling_directory
 
     # Read and degrade mask
     print("Reading mask")
-    mask = hp.ud_grade(hp.read_map(man.fname_mask), nside_out=man.nside)
+    mask = meta.read_mask("analysis")
+
     # Create dummy NaMaster field
-    f = nmt.NmtField(mask, None, spin=2)
+    field_spin0 = nmt.NmtField(mask, None, spin=0)
+    field_spin2 = nmt.NmtField(mask, None, spin=2)
+    
     # Binning scheme is irrelevant for us, but NaMaster needs one.
-    b = man.get_nmt_bins()
+    nmt_bins = meta.read_nmt_binning()
 
     # Alright, compute and reshape coupling matrix.
     # Compute also the binned MCM (this is actually what we need).
     print("Computing MCM")
     w = nmt.NmtWorkspace()
-    w.compute_coupling_matrix(f, f, b)
+    w.compute_coupling_matrix(field_spin0, field_spin2, nmt_bins, is_teb=True)
+
+    nl = meta.lmax
+    nspec = 7
     mcm = np.transpose(w.get_coupling_matrix().reshape([nl, nspec, nl, nspec]),
                        axes=[1, 0, 3, 2])
 
     # Save to file
-    fname_out = man.get_filename('mcm', o.output_dir)
-    print(f"Saving to {fname_out}")
-    np.savez(fname_out, mcm=mcm)
+    fname = f"{coupling_dir}/mcm.npz"
+    np.savez(fname, mcm=mcm, spin0xspin0=mcm[0,:,0,:], spin0xspin2=mcm[1:3,:,1:3,:], spin2xspin2=mcm[3:,:,3:,:])
 
-    if o.plot:
+    if args.plot:
         import matplotlib.pyplot as plt
 
         print("Plotting")
         plt.figure()
         plt.imshow(mcm.reshape([nspec*nl, nspec*nl]))
         plt.colorbar()
-        fname = os.path.join(man.get_filename("mcm_plots", o.output_dir),
-                             'mcm.pdf')
+        fname = f"{coupling_dir}/mcm.png"
         plt.savefig(fname, bbox_inches='tight')
 
 
@@ -54,5 +66,6 @@ if __name__ == '__main__':
     parser.add_argument("--output-dir", type=str, help='Output directory')
     parser.add_argument("--plot", action='store_true',
                         help='Pass to generate a plot of the MCM.')
-    o = parser.parse_args()
-    mcmer(o)
+    args = parser.parse_args()
+
+    mcmer(args)
