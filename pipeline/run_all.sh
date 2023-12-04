0af20269711045c6712711a277ff48a0260100a0
Kevin Wolz <kevin.wolz93@gmail.com>
2023-12-04 06:48:19 -0800

    clean up paramfiles and run scripts

--- pipeline/run_all.sh
+++ pipeline/run_all.sh
@@ -2,13 +2,25 @@ paramfile='<path-to-SOOPERCOOL>/paramfiles/paramfile_SAT.yaml'
 
 echo "Running pipeline with paramfile: ${paramfile}"
 
-echo "Running mask stage..."
-echo "---------------------"
-python mask_handler.py --globals ${paramfile} --plots
+# Un-comment the lines below when using use micromamba environments at NERSC
+# (for how-to, see INSTALL.md).
+# NOTE: TOAST3 needs the newest python version and is currently incompatible
+# with the conda-forge version of NaMaster (needs python version <= 3.6)
+# within a conda / micromamba environment. This workaround loads and unloads
+# two environments; one with NaMaster and one with TOAST3.
+
+# echo "Switching to environment with NaMaster"
+# eval "$(micromamba shell hook --shell bash)"
+# micromamba activate <namaster_env>
+
+echo "------------------------------------------------------------"
+echo "|           PREPARING METADATA AND SIMULATIONS             |"
+echo "------------------------------------------------------------"
 
 echo "Pre-processing data..."
 echo "-------------------"
-python pre_processer.py --globals ${paramfile} --sims 
+python pre_processer.py --globals ${paramfile} --sims
+python mask_handler.py --globals ${paramfile}
 
 echo "Running mock stage for data..."
 echo "------------------------------"
@@ -21,6 +33,14 @@ python mocker.py --globals ${paramfile} --sims
 echo "Running mcm..."
 echo "--------------"
 python mcmer.py --globals ${paramfile} --plot
+
+
+echo "------------------------------------------------------------"
+echo "|             FILTERING SIMULATIONS AND DATA               |"
+echo "------------------------------------------------------------"
+
+# echo "Switching to environment with TOAST"
+# micromamba deactivate; micromamba activate <toast_env>
 
 echo "Running filterer for transfer"
 echo "-----------------------------"
@@ -39,6 +59,14 @@ echo "-------------------------"
 python filterer.py --globals ${paramfile} --data
 #OMP_NUM_THREADS=2 mpirun -n 4 python filterer.py --globals ${paramfile} --data
 
+
+echo "------------------------------------------------------------"
+echo "|                 COMPUTING POWER SPECTRA                  |"
+echo "------------------------------------------------------------"
+
+# echo "Switching to environment with NaMaster"
+# micromamba deactivate; micromamba activate <namaster_env>
+
 echo "Running cl estimation for tf estimation"
 echo "---------------------------------------"
 python pcler.py --globals ${paramfile} --tf_est
@@ -54,8 +82,6 @@ python pcler.py --globals ${paramfile} --tf_val
 echo "Transfer validation"
 echo "---------------------"
 python transfer_validator.py --globals ${paramfile}
-
-
 
 echo "Running pcler on data"
 echo "---------------------"
