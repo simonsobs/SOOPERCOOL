999e3046cc79c65320e3575e572fabc65f261216
Kevin Wolz <kevin.wolz93@gmail.com>
2024-02-18 00:32:00 +0000

    consistent fsky, loading beam

--- pipeline/pre_processer.py
+++ pipeline/pre_processer.py
@@ -11,7 +11,12 @@ def pre_processer(args):
     """
     meta = BBmeta(args.globals)
 
-    # First step : create bandpower edges / binning_file
+    # Create beams for each map set
+    meta.timer.start("Generating beams")
+    utils.get_beam_windows(meta)
+    meta.timer.stop("Generating beams")
+
+    # Create bandpower edges / binning_file
     bin_low, bin_high, bin_center = utils.create_binning(meta.nside,
                                                          meta.deltal)
     np.savez(meta.path_to_binning, bin_low=bin_low, bin_high=bin_high,
@@ -27,7 +32,7 @@ def pre_processer(args):
         plt.title('Binning', fontsize=14)
         plt.savefig(meta.path_to_binning.replace('.npz', '.png'))
 
-    lmax_sim = 3*meta.nside-1
+    lmax_sim = 3*meta.nside - 1
 
     meta.timer.start("Computing fiducial cls")
 
@@ -48,26 +53,6 @@ def pre_processer(args):
             plt.ylabel(r'$\ell(\ell+1)\, C_\ell/(2\pi)$', fontsize=14)
             plt.title(f'cosmo_cls_{field_pair}')
             plt.savefig(cl_cosmo_fname.replace('.npz', f'_{field_pair}.png'))
-
-    # Create the fiducial noise cl
-    lth, nlth = utils.get_noise_cls(
-        meta.noise,
-        lmax=lmax_sim,
-        fsky=meta.noise["fsky"],
-        is_beam_deconvolved=True
-    )
-    cl_noise_fname = meta.save_fiducial_cl(lth, nlth, cl_type="noise")
-
-    if args.plots:
-        for field_pair in ["TT", "BB"]:
-            plt.figure(figsize=(8, 6))
-            dlth = lth*(lth + 1) / 2. / np.pi * nlth[field_pair]
-            plt.loglog(lth, dlth, c='b')
-            plt.loglog(lth[dlth < 0], -dlth[dlth < 0], ls='--', c='b')
-            plt.xlabel(r'$\ell$', fontsize=14)
-            plt.ylabel(r'$\ell(\ell+1)\, C_\ell/(2\pi)$', fontsize=14)
-            plt.title(f'noise_cls_{field_pair}')
-            plt.savefig(cl_noise_fname.replace('.npz', f'_{field_pair}.png'))
 
     # Create the fiducial power law spectra
     # Let's start with the power law used to compute
@@ -106,7 +91,7 @@ def pre_processer(args):
             plt.title(f'tf_validation_{field_pair}')
             plt.savefig(cl_tf_val_fname.replace('.npz',
                                                 f'_{field_pair}.png'))
-    meta.timer.stop("Computing fiducial cls", args.verbose)
+    meta.timer.stop("Computing fiducial cls")
 
     if args.sims:
         # Now we iterate over the number of simulations
@@ -125,60 +110,13 @@ def pre_processer(args):
                     [cls[k] for k in hp_ordering],
                     lmax=lmax_sim,
                 )
-
-                if cl_type == "tf_est":
-                    for case in ["pureE", "pureB"]:
-                        if case == "pureE":
-                            sim = hp.alm2map([alms_T, alms_E, alms_B*0.],
-                                             meta.nside, lmax=lmax_sim)
-                        elif case == "pureB":
-                            sim = hp.alm2map([alms_T, alms_E*0., alms_B],
-                                             meta.nside, lmax=lmax_sim)
-                        map_file = meta.get_map_filename_transfer2(
-                            id_sim, cl_type, pure_type=case
-                        )
-                        hp.write_map(map_file, sim, overwrite=True,
-                                     dtype=np.float32)
-
-                        if args.plots:
-                            fname = map_file.replace('.fits', '')
-                            title = map_file.split('/')[-1].replace('.fits',
-                                                                    '')
-                            amp = meta.power_law_pars_tf_est['amp']
-                            delta_ell = meta.power_law_pars_tf_est['delta_ell']
-                            pl_index = meta.power_law_pars_tf_est['power_law_index'] # noqa
-                            ell0 = 0 if pl_index > 0 else 2 * meta.nside
-                            var = amp / (ell0 + delta_ell)**pl_index
-                            utils.plot_map(sim, fname, vrange_T=10*var**0.5,
-                                           vrange_P=10*var**0.5, title=title,
-                                           TQU=True)
-
-                else:
-                    sim = hp.alm2map([alms_T, alms_E, alms_B],
-                                     meta.nside, lmax=lmax_sim)
-                    map_file = meta.get_map_filename_transfer2(id_sim, cl_type)
-                    hp.write_map(map_file, sim, overwrite=True,
-                                 dtype=np.float32)
-                    if args.plots:
-                        fname = map_file.replace('.fits', '')
-                        title = map_file.split('/')[-1].replace('.fits', '')
-                        if cl_type == "tf_val":
-                            amp_T = meta.power_law_pars_tf_val['amp']['TT']
-                            amp_E = meta.power_law_pars_tf_val['amp']['EE']
-                            delta_ell = meta.power_law_pars_tf_val['delta_ell']
-                            pl_index = meta.power_law_pars_tf_val['power_law_index'] # noqa
-                            ell0 = 0 if pl_index > 0 else 2 * meta.nside
-                            var_T = amp_T / (ell0 + delta_ell)**pl_index
-                            var_P = amp_E / (ell0 + delta_ell)**pl_index
-                            utils.plot_map(sim, fname, vrange_T=100*var_T**0.5,
-                                           vrange_P=100*var_P**0.5,
-                                           title=title, TQU=True)
-                        elif cl_type == "cosmo":
-                            utils.plot_map(sim, fname, title=title, TQU=True)
+                utils.generate_simulated_maps(meta, id_sim, cl_type,
+                                              alms_T, alms_E, alms_B,
+                                              args.plots)
 
             meta.timer.stop("Generate `cosmo` and `power_law` simulation "
-                            f"n° {id_sim:04d}", args.verbose)
-        meta.timer.stop("Generating simulations for transfer", args.verbose)
+                            f"n° {id_sim:04d}")
+        meta.timer.stop("Generating simulations for transfer")
 
 
 if __name__ == "__main__":
