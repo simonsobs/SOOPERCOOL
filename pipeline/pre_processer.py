d27f1d47ad8a2c9f115664adf4bffc11ab6b9c17
Adrien La Posta <adrien.la.posta@gmail.com>
2024-02-22 13:39:26 +0000

    generate beamed val and est sims

--- pipeline/pre_processer.py
+++ pipeline/pre_processer.py
@@ -91,6 +91,17 @@ def pre_processer(args):
         # Now we iterate over the number of simulations
         # to generate maps for each of them
         hp_ordering = ["TT", "TE", "TB", "EE", "EB", "BB"]
+
+        filtering_tags = meta.get_filtering_tags()
+        beams = []
+        for ftag in filtering_tags:
+            which_beam = meta.tags_settings[ftag]["beam"]
+            if which_beam is not None:
+                _, bl = meta.read_beam(which_beam)
+            else:
+                bl = None
+            beams.append((ftag, bl))
+
         meta.timer.start("Generating simulations for transfer")
         for id_sim in range(meta.tf_est_num_sims):
             meta.timer.start("Generate `cosmo` and `power_law` "
@@ -106,54 +117,80 @@ def pre_processer(args):
                 )
 
                 if cl_type == "tf_est":
-                    for case in ["pureE", "pureB"]:
-                        if case == "pureE":
-                            sim = hp.alm2map([alms_T, alms_E, alms_B*0.],
-                                             meta.nside, lmax=lmax_sim)
-                        elif case == "pureB":
-                            sim = hp.alm2map([alms_T, alms_E*0., alms_B],
-                                             meta.nside, lmax=lmax_sim)
-                        map_file = meta.get_map_filename_transfer2(
-                            id_sim, cl_type, pure_type=case
+                    for ftag, bl in beams:
+
+                        if bl is not None:
+                            Tlm = hp.almxfl(alms_T, bl)
+                            Elm = hp.almxfl(alms_E, bl)
+                            Blm = hp.almxfl(alms_B, bl)
+                        else:
+                            Tlm, Elm, Blm = alms_T, alms_E, alms_B
+
+                        for case in ["pureE", "pureB"]:
+                            if case == "pureE":
+                                sim = hp.alm2map([Tlm, Elm, Blm*0.],
+                                                 meta.nside, lmax=lmax_sim)
+                            elif case == "pureB":
+                                sim = hp.alm2map([Tlm, Elm*0., Blm],
+                                                 meta.nside, lmax=lmax_sim)
+                            map_file = meta.get_map_filename_transfer(
+                                id_sim, cl_type, pure_type=case,
+                                filter_tag=ftag
+                            )
+                            hp.write_map(map_file, sim, overwrite=True,
+                                         dtype=np.float32)
+
+                            if args.plots:
+                                fname = map_file.replace('.fits', '')
+                                title = map_file.split('/')[-1].replace('.fits',
+                                                                        '')
+                                amp = meta.power_law_pars_tf_est['amp']
+                                delta_ell = meta.power_law_pars_tf_est['delta_ell']
+                                pl_index = meta.power_law_pars_tf_est['power_law_index'] # noqa
+                                ell0 = 0 if pl_index > 0 else 2 * meta.nside
+                                var = amp / (ell0 + delta_ell)**pl_index
+                                utils.plot_map(
+                                    sim, fname, vrange_T=10*var**0.5,
+                                    vrange_P=10*var**0.5, title=title,
+                                    TQU=True
+                                )
+
+                else:
+                    for ftag, bl in beams:
+                        
+                        if bl is not None:
+                            Tlm = hp.almxfl(alms_T, bl)
+                            Elm = hp.almxfl(alms_E, bl)
+                            Blm = hp.almxfl(alms_B, bl)
+                        else:
+                            Tlm, Elm, Blm = alms_T, alms_E, alms_B
+
+                        sim = hp.alm2map([Tlm, Elm, Blm],
+                                         meta.nside, lmax=lmax_sim)
+                        map_file = meta.get_map_filename_transfer(
+                            id_sim, cl_type,
+                            filter_tag=ftag
                         )
                         hp.write_map(map_file, sim, overwrite=True,
                                      dtype=np.float32)
-
                         if args.plots:
                             fname = map_file.replace('.fits', '')
-                            title = map_file.split('/')[-1].replace('.fits',
-                                                                    '')
-                            amp = meta.power_law_pars_tf_est['amp']
-                            delta_ell = meta.power_law_pars_tf_est['delta_ell']
-                            pl_index = meta.power_law_pars_tf_est['power_law_index'] # noqa
-                            ell0 = 0 if pl_index > 0 else 2 * meta.nside
-                            var = amp / (ell0 + delta_ell)**pl_index
-                            utils.plot_map(sim, fname, vrange_T=10*var**0.5,
-                                           vrange_P=10*var**0.5, title=title,
-                                           TQU=True)
-
-                else:
-                    sim = hp.alm2map([alms_T, alms_E, alms_B],
-                                     meta.nside, lmax=lmax_sim)
-                    map_file = meta.get_map_filename_transfer2(id_sim, cl_type)
-                    hp.write_map(map_file, sim, overwrite=True,
-                                 dtype=np.float32)
-                    if args.plots:
-                        fname = map_file.replace('.fits', '')
-                        title = map_file.split('/')[-1].replace('.fits', '')
-                        if cl_type == "tf_val":
-                            amp_T = meta.power_law_pars_tf_val['amp']['TT']
-                            amp_E = meta.power_law_pars_tf_val['amp']['EE']
-                            delta_ell = meta.power_law_pars_tf_val['delta_ell']
-                            pl_index = meta.power_law_pars_tf_val['power_law_index'] # noqa
-                            ell0 = 0 if pl_index > 0 else 2 * meta.nside
-                            var_T = amp_T / (ell0 + delta_ell)**pl_index
-                            var_P = amp_E / (ell0 + delta_ell)**pl_index
-                            utils.plot_map(sim, fname, vrange_T=100*var_T**0.5,
-                                           vrange_P=100*var_P**0.5,
-                                           title=title, TQU=True)
-                        elif cl_type == "cosmo":
-                            utils.plot_map(sim, fname, title=title, TQU=True)
+                            title = map_file.split('/')[-1].replace('.fits', '')
+                            if cl_type == "tf_val":
+                                amp_T = meta.power_law_pars_tf_val['amp']['TT']
+                                amp_E = meta.power_law_pars_tf_val['amp']['EE']
+                                delta_ell = meta.power_law_pars_tf_val['delta_ell']
+                                pl_index = meta.power_law_pars_tf_val['power_law_index'] # noqa
+                                ell0 = 0 if pl_index > 0 else 2 * meta.nside
+                                var_T = amp_T / (ell0 + delta_ell)**pl_index
+                                var_P = amp_E / (ell0 + delta_ell)**pl_index
+                                utils.plot_map(
+                                    sim, fname, vrange_T=100*var_T**0.5,
+                                    vrange_P=100*var_P**0.5,
+                                    title=title, TQU=True
+                                )
+                            elif cl_type == "cosmo":
+                                utils.plot_map(sim, fname, title=title, TQU=True)
 
             meta.timer.stop("Generate `cosmo` and `power_law` simulation "
                             f"n° {id_sim:04d}", args.verbose)
