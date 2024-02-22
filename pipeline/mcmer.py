ff48b8eec1a39824e6cb2653baacb4c72b4c4406
Adrien La Posta <adrien.la.posta@gmail.com>
2024-02-22 13:40:53 +0000

    update mcmer

--- pipeline/mcmer.py
+++ pipeline/mcmer.py
@@ -107,15 +107,44 @@ def mcmer(args):
 
     # Then the beamed MCM
     for map_set1, map_set2 in meta.get_ps_names_list("all", coadd=True):
-        mcm = beamed_mcm[map_set1, map_set2]
-        mcm_binned = np.einsum('ij,kjlm->kilm', binner, mcm)
+        m = beamed_mcm[map_set1, map_set2]
+        mcm_binned = np.einsum('ij,kjlm->kilm', binner, m)
         fname = f"{coupling_dir}/mcm_{map_set1}_{map_set2}.npz"
         np.savez(
             fname,
             binner=binner,
-            spin0xspin0=mcm[0, :, 0, :].reshape([1, nl, 1, nl]),
-            spin0xspin2=mcm[1:3, :, 1:3, :],
-            spin2xspin2=mcm[3:, :, 3:, :],
+            spin0xspin0=m[0, :, 0, :].reshape([1, nl, 1, nl]),
+            spin0xspin2=m[1:3, :, 1:3, :],
+            spin2xspin2=m[3:, :, 3:, :],
+            spin0xspin0_binned=mcm_binned[0, :, 0, :].reshape([1, n_bins,
+                                                               1, nl]),
+            spin0xspin2_binned=mcm_binned[1:3, :, 1:3, :],
+            spin2xspin2_binned=mcm_binned[3:, :, 3:, :]
+        )
+
+    # Finally, for the different filtering tag pairs.
+    # used for the transfer validation stage, in
+    # case we beamed the estimation and validation simulations
+    for ftag1, ftag2 in meta.get_independent_filtering_pairs():
+        
+        beam_label1 = meta.tags_settings[ftag1]["beam"]
+        beam_label2 = meta.tags_settings[ftag2]["beam"]
+
+        bl1 = beams[beam_label1] if beam_label1 is not None else np.ones(nl)
+        bl2 = beams[beam_label2] if beam_label2 is not None else np.ones(nl)
+
+        beamed_mcm = mcm * \
+            np.outer(bl1, bl2)[np.newaxis, :, np.newaxis, :]
+        
+        mcm_binned = np.einsum('ij,kjlm->kilm', binner, beamed_mcm)
+
+        fname = f"{coupling_dir}/mcm_{ftag1}_{ftag2}.npz"
+        np.savez(
+            fname,
+            binner=binner,
+            spin0xspin0=beamed_mcm[0, :, 0, :].reshape([1, nl, 1, nl]),
+            spin0xspin2=beamed_mcm[1:3, :, 1:3, :],
+            spin2xspin2=beamed_mcm[3:, :, 3:, :],
             spin0xspin0_binned=mcm_binned[0, :, 0, :].reshape([1, n_bins,
                                                                1, nl]),
             spin0xspin2_binned=mcm_binned[1:3, :, 1:3, :],
