33b106cb52801bfc41b2f788ebddd4709708a1a0
Kevin Wolz <kevin.wolz93@gmail.com>
2024-07-13 09:39:56 -0700

    plot full-sky cls for cmb sims

--- pipeline/simulations/generate_mock_cmb_sky.py
+++ pipeline/simulations/generate_mock_cmb_sky.py
@@ -2,7 +2,7 @@ import argparse
 from soopercool import BBmeta
 import healpy as hp
 from soopercool import utils
-from soopercool import mpi_utils as mpi
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -14,7 +14,9 @@ def main(args):
     out_dir = meta.output_directory
 
     sims_dir = f"{out_dir}/cmb_sims"
+    plot_dir = f"{out_dir}/plots/cmb_sims"
     BBmeta.make_dir(sims_dir)
+    BBmeta.make_dir(plot_dir)
 
     lmax_sim = 3*meta.nside - 1
 
@@ -33,9 +35,7 @@ def main(args):
 
     hp_ordering = ["TT", "TE", "TB", "EE", "EB", "BB"]
 
-    mpi.init(True)
-
-    for id_sim in mpi.taskrange(meta.covariance["cov_num_sims"] - 1):
+    for id_sim in range(meta.covariance["cov_num_sims"]):
         alms = hp.synalm([clth[fp] for fp in hp_ordering])
         for ms in meta.map_sets_list:
             if verbose:
@@ -44,13 +44,40 @@ def main(args):
             alms_beamed = [hp.almxfl(alm, beams[ms]) for alm in alms]
 
             map = hp.alm2map(alms_beamed, nside=meta.nside)
-
-            import matplotlib.pyplot as plt
-            hp.mollview(map[1], title=f"{ms} - {id_sim}")
-            plt.show()
-
             hp.write_map(f"{sims_dir}/cmb_{ms}_{id_sim:04d}.fits", map,
                          overwrite=True, dtype=np.float32)
+            hp.write_cl(f"{sims_dir}/cl_{ms}_{id_sim:04d}.fits",
+                        hp.anafast(map), overwrite=True, dtype=np.float32)
+
+    # Plotting
+    cls = {ms: [] for ms in meta.map_sets_list}
+    for ms in meta.map_sets_list:
+        for id_sim in range(meta.covariance["cov_num_sims"]):
+            cls[ms].append(
+                hp.read_cl(f"{sims_dir}/cl_{ms}_{id_sim:04d}.fits")
+            )
+    cl_mean = {ms: np.mean(np.array(cls[ms]), axis=0)
+               for ms in meta.map_sets_list}
+    cl_std = {ms: np.std(np.array(cls[ms]), axis=0)
+              for ms in meta.map_sets_list}
+
+    ll = np.arange(lmax_sim + 1)
+    cl2dl = ll*(ll + 1)/2./np.pi
+    for ms in meta.map_sets_list:
+        for ip, fp in enumerate(["TT", "EE", "BB", "TE"]):
+            plt.title(f"{ms} | {fp}")
+            plt.errorbar(ll, cl2dl*cl_mean[ms][ip], cl2dl*cl_std[ms][ip],
+                         label="data", color="navy", lw=0.5, zorder=-32)
+            plt.plot(ll, cl2dl*clth[fp], label=" theory",
+                     color="k", ls="--")
+            plt.plot(ll, cl2dl*clth[fp]*beams[ms]**2, label=" beamed theory",
+                     color="darkorange", ls="--")
+            plt.ylabel(r"$D_\ell$")
+            plt.xlabel(r"$\ell$")
+            plt.yscale("log")
+            plt.legend()
+            plt.savefig(f"{plot_dir}/cl_{ms}_{fp}.png")
+            plt.clf()
 
 
 if __name__ == "__main__":
