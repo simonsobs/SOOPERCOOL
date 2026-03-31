49096e65f0b57ddeda3872e30a15b64e79b15f4a
Adrien La Posta <adrien.la.posta@gmail.com>
2026-03-31 11:51:36 +0100

    fft filtering

---
+++ pipeline/kspace/filter_sims_kspace.py
@@ -0,0 +1,85 @@
+from soopercool import fft_utils as sfft
+from soopercool import mpi_utils as mpi
+from soopercool import map_utils as mu
+from soopercool import BBmeta
+import argparse
+import os
+
+
+def main(args):
+    """ """
+
+    rank, size, comm = mpi.init(True)
+
+    meta = BBmeta(args.globals)
+
+    out_dir = f"{meta.output_directory}/kspace_filtered_sims"
+    meta.make_dir(out_dir)
+
+    tf_settings = meta.transfer_settings
+    id_start, n_sims = (
+        tf_settings["sim_id_start"],
+        tf_settings["tf_est_num_sims"],
+    )
+    pure_types = [f"pure{f}" for f in "TEB"]
+
+    if rank == 0:
+        files_list = []
+        for map_set in meta.map_sets:
+            print(f"Processing map set: {map_set}")
+
+            kspace_tag = meta.kspace_tag_from_map_set(map_set)
+            ftag = meta.filtering_tag_from_map_set(map_set)
+            if kspace_tag is not None:
+                print(f"  Applying k-space filter: {kspace_tag}")
+                kspace_pars = tf_settings["kspace_pars"][kspace_tag]
+                print(f"  k-space filter parameters: {kspace_pars}")
+
+            else:
+                print(
+                    f"  No k-space filter to be applied to map set {map_set}. Skipping." # noqa
+                )
+
+            map_dir = tf_settings["filtered_map_dir"][ftag]
+            for id_sim in range(id_start, id_start + n_sims):
+                for pure_type in pure_types:
+                    fname = tf_settings["filtered_map_template"][ftag].format(
+                        pure_type=pure_type, id_sim=id_sim
+                    )
+                    path = f"{map_dir}/{fname}"
+                    print(os.path.split(path)[-1])
+                    files_list.append((path, kspace_pars))
+
+    # Every rank must have the same list order
+    mpi_shared_list = comm.bcast(files_list, root=0)
+
+    task_ids = mpi.distribute_tasks(size, rank, len(files_list))
+    local_files_list = [mpi_shared_list[i] for i in task_ids]
+
+    for map_fname, kspace_pars in local_files_list:
+
+        m = mu.read_map(
+            map_fname,
+            pix_type=meta.pix_type,
+            car_template=meta.car_template,
+            fields_hp=[0, 1, 2],
+        )
+
+        m_filtered = sfft.kspace_filter(m, pix_type="car", **kspace_pars)
+
+        mu.write_map(
+            f"{out_dir}/{os.path.split(map_fname)[-1]}",
+            m_filtered,
+            pix_type=meta.pix_type,
+            car_template=meta.car_template,
+        )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument(
+        "--globals", help="Path to the soopercool parameter file"
+    )
+    args = parser.parse_args()
+
+    main(args)
--- soopercool/fft_utils.py
+++ soopercool/fft_utils.py
@@ -2,39 +2,67 @@ import numpy as np
 from pixell import enmap
 
 
-def kspace_filter(m, dkx=0, dky=0, type="sharp"):
+def kspace_filter(m, dkx=0, dky=0, type="sharp", pix_type="car"):
     """
-    Filter the Fourier space of the map `m` by zeroing out modes with |kx| < dkx or |ky| < dky.
+    Filter the Fourier space of the map `m`
 
     Parameters
     ----------
     m : enmap
         The input map to be filtered.
     dkx : float, optional
-        The cutoff in the kx direction. Modes with |kx| < dkx will be filtered out. Default is 0 (no filtering).
+        The cutoff in the kx direction.
+        Modes with |kx| < dkx will be filtered out.
+        Default is 0 (no filtering).
     dky : float, optional
-        The cutoff in the ky direction. Modes with |ky| < dky will be filtered out. Default is 0 (no filtering).
+        The cutoff in the ky direction.
+        Modes with |ky| < dky will be filtered out.
+        Default is 0 (no filtering).
     type : str, optional
-        The type of filter to apply. Options are "sharp" (default) for a hard cutoff, and "cosine" for a smooth cosine taper.
+        The type of filter to apply. 
+        Options are "sharp" (default) for a hard cutoff
+        and "cosine" for a smooth cosine taper.
     """
-    ky, kx = m.lmap()
-    mf = enmap.fft(m)
+    if pix_type == "hp":
+        raise NotImplementedError(
+            "k-space filtering is currently only implemented for CAR maps."
+        )
 
-    if type == "sharp":
-        msk_x = np.abs(kx) < dkx
-        msk_y = np.abs(ky) < dky
-        msk = msk_x | msk_y
-        mf[:, msk] = 0.
-    if type == "cosine":
-        w_x = cosine_taper(np.abs(kx), 0, dkx)
-        w_y = cosine_taper(np.abs(ky), 0, dky)
-        mf *= w_x * w_y
-    mf_back = enmap.ifft(mf)
-    return mf_back
+    elif pix_type == "car":
+        ky, kx = m.lmap()
+        mf = enmap.fft(m)
+
+        if type == "sharp":
+            msk_x = np.abs(kx) < dkx
+            msk_y = np.abs(ky) < dky
+            msk = msk_x | msk_y
+            mf[:, msk] = 0.0
+        if type == "cosine":
+            w_x = cosine_taper(np.abs(kx), 0, dkx)
+            w_y = cosine_taper(np.abs(ky), 0, dky)
+            mf *= w_x * w_y
+        mf_back = enmap.ifft(mf)
+        return mf_back
 
 
 def cosine_taper(k, kmin, kmax):
     """
+    Utility function to create a cosine taper filter in Fourier space.
+    Transition smoothly from 0 to 1 between kmin and kmax.
+
+    Parameters
+    ----------
+    k : numpy.ndarray
+        The wavenumber array.
+    kmin : float
+        The minimum wavenumber for the taper.
+    kmax : float
+        The maximum wavenumber for the taper.
+
+    Returns
+    -------
+    numpy.ndarray
+        The taper filter.
     """
     if kmax == kmin:
         return np.ones_like(k)
