Fourier filtering, covariance fixes and masks (#70)
* adapt to multi hit maps

* fix binning

* fix bin

* some fft tools

* fft filtering

* add todo mask

* fix output naming

* adapt to multi-filtering

* fix fname typo

* on-the-fly filtering in cl estimation

* todo

* testing is useful

* fix fft

* coupling updates

* pcls fix and mask

* flake8

* fix cov

* Squashed commit of the following:

commit ac76d04772bc6f9bee12b1dc3af10b1a3d182cf9
Author: kwolz <kevin.wolz93@gmail.com>
Date:   Sat Mar 14 19:59:24 2026 -0400

    remove unnecessary line

commit 50c011f3aacfcddf2555ebd76f6ed54f2a420953
Author: kwolz <kevin.wolz93@gmail.com>
Date:   Sat Mar 14 19:56:58 2026 -0400

    removed unused script

commit ffefc79f1330b342caf5c29e13b2c9954fce7fc9
Author: kwolz <kevin.wolz93@gmail.com>
Date:   Sat Mar 14 19:45:43 2026 -0400

    lint

commit b9e9116d1f173399a178d1f4eff2fa81646551d1
Merge: 3cd5550 6596179
Author: kwolz <kevin.wolz93@gmail.com>
Date:   Sat Mar 14 19:42:10 2026 -0400

    merging from remote

commit 3cd5550a77bc96e8b76c3cb8d10fcf991e22128d
Author: kwolz <kevin.wolz93@gmail.com>
Date:   Sat Mar 14 19:34:14 2026 -0400

    add mask validation script, small refactoring of map_utils

commit b17450d55df1e65610af26a179b37935252ae54a
Author: kwolz <kevin.wolz93@gmail.com>
Date:   Sat Mar 14 19:31:50 2026 -0400

    refactor & doc get_analysis_mask

commit 7248903f155dd86a1f1bc67bda2b9c7eba266443
Merge: 0ce6c85 329a7bc
Author: kwolz <kevin.wolz93@gmail.com>
Date:   Thu Feb 19 13:19:03 2026 -0500

    resolve merger conflicts

commit 329a7bccef1585830189e56cb12dc4b6090f6469
Author: Adrien La Posta <adrien.la.posta@gmail.com>
Date:   Wed Feb 18 17:44:46 2026 +0000

    first commit, clean smooth mask

commit 6596179b76d1388ee78cedad568c7a5c560a5e40
Author: kwolz <kevin.wolz93@gmail.com>
Date:   Sun Sep 28 14:41:24 2025 +0100

    improve plot transfer function

commit da91814696498507b434a1e267a41d5383e5ad6a
Author: kwolz <kevin.wolz93@gmail.com>
Date:   Sun

pipeline/prepare_cov_inputs.py
@@ -142,6 +142,8 @@ def main(args):
         # already been generated for transfer function estimation
         ftag1 = meta.filtering_tag_from_map_set(map_set1)
         ftag2 = meta.filtering_tag_from_map_set(map_set2)
+        ktag1 = meta.kspace_tag_from_map_set(map_set1)
+        ktag2 = meta.kspace_tag_from_map_set(map_set2)
 
         sim_ids = range(
             tf_settings["sim_id_start"],
@@ -153,12 +155,12 @@ def main(args):
         for sim_id in sim_ids:
             ps_mat_filtered.append(
                 np.load(
-                    f"{cells_tf_dir}/pcls_mat_tf_est_{ftag1}_x_{ftag2}_filtered_unbinned_{sim_id:04d}.npz" # noqa
+                    f"{cells_tf_dir}/pcls_mat_tf_est_{ftag1}_{ktag1}_x_{ftag2}_{ktag2}_filtered_unbinned_{sim_id:04d}.npz" # noqa
                 )["pcls_mat"]
             )
             ps_mat_unfiltered.append(
                 np.load(
-                    f"{cells_tf_dir}/pcls_mat_tf_est_{ftag1}_x_{ftag2}_unfiltered_unbinned_{sim_id:04d}.npz" # noqa
+                    f"{cells_tf_dir}/pcls_mat_tf_est_{ftag1}_{ktag1}_x_{ftag2}_{ktag2}_unfiltered_unbinned_{sim_id:04d}.npz" # noqa
                 )["pcls_mat"]
             )
 
@@ -197,7 +199,7 @@ def main(args):
                 x,
                 y,
                 [0, 2, 6, 10, 30, 50, 70, 90, 100, 200,
-                 300, 400, 500, 600, 700, 800, 900, nl]
+                 300, 400, 500, 600, 700, nl]  # TODO: fix this
             )
             msk_fit = (x >= 2) & (x <= 650)
             corr = gp_fit(

soopercool/coupling_utils.py
@@ -87,7 +87,9 @@ def read_pcls_matrices(pcls_mat_dir, filtering_pairs, Nsims, sim_id_start=0):
     for id_sim in range(sim_id_start, Nsims + sim_id_start):
         for label in ["filtered", "unfiltered"]:
             for ftag1, ftag2 in filtering_pairs:
-                suffix = f"{ftag1}_x_{ftag2}_{label}_{id_sim:04d}"
+                lab1 = f"{ftag1[0]}_{ftag1[1]}"
+                lab2 = f"{ftag2[0]}_{ftag2[1]}"
+                suffix = f"{lab1}_x_{lab2}_{label}_{id_sim:04d}"
                 pcls_mat = np.load(
                     f"{pcls_mat_dir}/pcls_mat_tf_est_{suffix}.npz")
                 pcls_mat_dict[ftag1, ftag2][label] += [pcls_mat["pcls_mat"]]
@@ -244,7 +246,6 @@ def compute_couplings(mcm, nmt_binning, transfer=None, compute_Dl=False):
 def get_couplings_dict(mcm_dict, nmt_binning,
                        transfer_dict=None,
                        ps_names_and_ftags=None,
-                       filtering_pairs=None,
                        compute_Dl=False):
     """
     Compute couplings from pre-computed mode-coupling
@@ -269,9 +270,6 @@ def get_couplings_dict(mcm_dict, nmt_binning,
         Dictionary with keys as (map_set1, map_set2) and
         values as (ftag1, ftag2).
         If provided, couplings will be computed for these pairs.
-    filtering_pairs : list of tuples, optional
-        List of (ftag1, ftag2) tuples. If provided, couplings
-        will be computed for these pairs.
     compute_Dl : bool, optional
         If True, applies the Dl conversion when computing the binned MCM.
         The code will then output power spectra in Dl units.
@@ -296,18 +294,9 @@ def get_couplings_dict(mcm_dict, nmt_binning,
             couplings[ms1, ms2]["inv_coupling"] = inv_coupling
 
     else:
-        for ftag1, ftag2 in filtering_pairs:
-            couplings[ftag1, ftag2] = {}
-            mcm = mcm_dict[ftag1, ftag2]
-            if transfer_dict is not None:
-                transfer = transfer_dict[ftag1, ftag2]
-            else:
-                transfer = None
-
-            bpw_win, inv_coupling = compute_couplings(
-                mcm, nmt_binning, transfer, compute_Dl=compute_Dl
-            )
-            couplings[ftag1, ftag2]["bp_win"] = bpw_win
-            couplings[ftag1, ftag2]["inv_coupling"] = inv_coupling
+        raise ValueError(
+            "Nothing computed. "
+            "Should provide a ps_names_and_ftags argument."
+        )
 
     return couplings

soopercool/metadata_manager.py
@@ -1,10 +1,10 @@
 import soopercool.map_utils as mu
+import soopercool.ps_utils as pu
 import soopercool.utils as su
 import yaml
 import numpy as np
 import os
 import time
-import pymaster as nmt
 
 
 class BBmeta(object):
@@ -49,8 +49,29 @@ class BBmeta(object):
         self.map_sets_list = self._get_map_sets_list()
         self.maps_list = self._get_map_list()
 
+        self._check_required_settings()
+
         # Initialize a timer
         self.timer = Timer()
+
+    def _check_required_settings(self):
+        """
+        Check that all required settings are present in the configuration
+        file `map_sets` section. If not, raises an error.
+        """
+        required_keys = [
+            "kspace_tag",
+            "hits_tag"
+        ]
+        for map_set in self.map_sets:
+            for key in required_keys:
+                if key not in self.map_sets[map_set]:
+                    raise KeyError(
+                        f"Missing required key {key} in map_sets"
+                        f" section for map_set {map_set}. Older"
+                        " versions of parameter files might be"
+                        " missing these keys, make sure to update them."
+                    )
 
     def _yaml_loader(self, config):
         """
@@ -221,35 +242,14 @@ class BBmeta(object):
             os.path.join(self.mask_directory, self.masks["nhits_map"]),
             map, dtype=np.float32, pix_type=self.pix_type)
 
-    def read_nmt_binning(self):
+    def read_nmt_binning(self, force_cl=False):
         """
         Read the binning file and return the corresponding NmtBin object.
         """
-        binning = np.load(self.binning_file)
-        bin_low, bin_high = binning["bin_low"], binning["bin_high"]
-        bin_lmax = bin_high[-1]
-
-        if bin_lmax < self.lmax:
-            raise ValueError(
-                f"lmax in binning {bin_lmax} is lower than {self.lmax}."
-                " Update config file to change lmax or binning scheme."
-            )
-        else:
-            # Truncate binning
-            select = bin_low < self.lmax
-            bin_low = bin_low[select]
-            bin_high = bin_high[select]
-
-            bin_high = np.concatenate((
-                bin_high[:-1],
-                np.array([self.lmax])
-            ))
-
-        return nmt.NmtBin.from_edges(
-            bin_low,
-            bin_high + 1,
-            is_Dell=self.compute_Dl
-        )
+        return pu.read_nmt_binning(self.binning_file,
+                                   self.lmax,
+                                   self.compute_Dl,
+                                   force_cl=force_cl)
 
     def get_n_bandpowers(self):
         """
@@ -678,10 +678,17 @@ class BBmeta(object):
     def get_filtering_tags(self):
         """
         """
-        return list(set(
-            [self.filtering_tag_from_map_set(ms)
-             for ms in self.map_sets_list]
-        ))
+        return list(
+            set(
+                [
+                    (
+                        self.filtering_tag_from_map_set(ms),
+                        self.kspace_tag_from_map_set(ms),
+                    )
+                    for ms in self.map_sets_list
+                ]
+            )
+        )
 
     def get_independent_filtering_pairs(self):
         """
@@ -691,7 +698,9 @@ class BBmeta(object):
         for ms1, ms2 in cross_ps_names:
             fp1 = self.filtering_tag_from_map_set(ms1)
             fp2 = self.filtering_tag_from_map_set(ms2)
-            filtering_pairs.append((fp1, fp2))
+            kt1 = self.kspace_tag_from_map_set(ms1)
+            kt2 = self.kspace_tag_from_map_set(ms2)
+            filtering_pairs.append(((fp1, kt1), (fp2, kt2)))
         return list(set(filtering_pairs))
 
     def get_inverse_couplings(self, return_bpwf=False):

soopercool/utils.py
@@ -7,13 +7,16 @@ from matplotlib import cm
 import camb
 
 
-def get_theory_cls(cosmo_params=None, lmax=4000, lmin=0, fwhm_amin=30):
+def get_theory_cls(cosmo_params=None, lmax=4000, lmin=0, fwhm_amin=30,
+                   verbose=True):
     """
     """
-    print("\nTheory C_ells:")
+    if verbose:
+        print("\nTheory C_ells:")
     if cosmo_params is None:
-        print("\n  WARNING: "
-              "Loading Planck standard CMB parameters with r=0, AL=1\n")
+        if verbose:
+            print("  WARNING: "
+                  "Loading Planck standard CMB parameters with r=0, AL=1")
         cosmo_params = {
             "cosmomc_theta": 0.0104085,
             "As": 2.1e-9,
@@ -24,7 +27,8 @@ def get_theory_cls(cosmo_params=None, lmax=4000, lmin=0, fwhm_amin=30):
             "tau": 0.0544,
             "r": 0.0,
         }
-    print(f"  Beam FWHM: {fwhm_amin} arcmin\n")
+    if verbose:
+        print(f"  Beam FWHM: {fwhm_amin} arcmin\n")
     params = camb.set_params(**cosmo_params)
     results = camb.get_results(params)
     powers = results.get_cmb_power_spectra(
@@ -505,7 +509,8 @@ def bin_validation_power_spectra(cls_dict, nmt_binning,
     return cls_binned_dict
 
 
-def plot_transfer_function(lb, tf_dict, lmin, lmax, field_pairs, file_name):
+def plot_transfer_function(lb, tf_dict, lmin, lmax, field_pairs,
+                           file_name=None):
     """
     Plot the transfer function given an input dictionary.
     """
@@ -513,112 +518,38 @@ def plot_transfer_function(lb, tf_dict, lmin, lmax, field_pairs, file_name):
     plt.figure(figsize=(25*npan/9, 25*npan/9))
     grid = plt.GridSpec(npan, npan, hspace=0.3, wspace=0.3)
 
-    for id1, f1 in enumerate(field_pairs):
-        for id2, f2 in enumerate(field_pairs):
-            ax = plt.subplot(grid[id1, id2])
-            expected = 1. if f1 == f2 else 0.
-            ylims = [0, 1.05] if f1 == f2 else [-0.01, 0.01]
+    for label, tf in tf_dict.items():
+        for id1, f1 in enumerate(field_pairs):
+            for id2, f2 in enumerate(field_pairs):
+                ax = plt.subplot(grid[id1, id2])
+                expected = 1. if f1 == f2 else 0.
+                ylims = [0, 1.05] if f1 == f2 else [-0.01, 0.01]
 
-            ax.axhline(expected, color="k", ls="--", zorder=6)
-            # We need to understand the offdigonal TF panels in the presence of
-            # NaMaster purification - we don't have a clear interpretation.
-            ax.set_title(f"{f1} $\\rightarrow$ {f2}", fontsize=14)
-            ax.plot(lb, tf_dict[f"{f1}_to_{f2}"], color="navy")
+                ax.axhline(expected, color="k", ls="--", zorder=6)
+                # We need to understand the offdigonal TF panels in the
+                # presence of NaMaster purification - we don't have a clear
+                # interpretation.
+                ax.set_title(f"{f1} $\\rightarrow$ {f2}", fontsize=14)
+                ax.plot(lb, tf[f"{f1}_to_{f2}"], label=label)
 
             if id1 == npan-1:
                 ax.set_xlabel(r"$\ell$", fontsize=14)
             else:
                 ax.set_xticks([])
 
-            if f1 != f2:
-                ax.ticklabel_format(axis="y", style="scientific",
-                                    scilimits=(0, 0), useMathText=True)
+                if f1 != f2:
+                    ax.ticklabel_format(axis="y", style="scientific",
+                                        scilimits=(0, 0), useMathText=True)
 
-            ax.set_xlim(lmin, lmax)
-            ax.set_ylim(ylims[0], ylims[1])
+                ax.set_xlim(lmin, lmax)
+                ax.set_ylim(ylims[0], ylims[1])
+                if label is not None and id1 == 0 and id2 == npan - 1:
+                    ax.legend(fontsize=10, bbox_to_anchor=[1, 1],
+                              loc="upper left")
 
     plt.savefig(file_name, bbox_inches="tight")
     plt.close()
     plt.clf()
-
-
-def get_binary_mask_from_nhits(nhits_map, nside, zero_threshold=1e-3):
-    """
-    Make binary mask by smoothing, normalizing and thresholding nhits map.
-    """
-    nhits_smoothed = hp.smoothing(
-        hp.ud_grade(nhits_map, nside, power=-2, dtype=np.float64),
-        fwhm=np.pi/180)
-    nhits_smoothed[nhits_smoothed < 0] = 0
-    nhits_smoothed /= np.amax(nhits_smoothed)
-    binary_mask = np.zeros_like(nhits_smoothed)
-    binary_mask[nhits_smoothed > zero_threshold] = 1
-
-    return binary_mask
-
-
-def get_apodized_mask_from_nhits(nhits_map, nside,
-                                 galactic_mask=None,
-                                 point_source_mask=None,
-                                 zero_threshold=1e-3,
-                                 apod_radius=10.,
-                                 apod_radius_point_source=4.,
-                                 apod_type="C1"):
-    """
-    Produce an appropriately apodized mask from an nhits map as used in
-    the BB pipeline paper (https://arxiv.org/abs/2302.04276).
-
-    Procedure:
-    * Make binary mask by smoothing, normalizing and thresholding nhits map
-    * (optional) multiply binary mask by galactic mask
-    * Apodize (binary * galactic)
-    * (optional) multiply (binary * galactic) with point source mask
-    * (optional) apodize (binary * galactic * point source)
-    * Multiply everything by (smoothed) nhits map
-    """
-    import pymaster as nmt
-
-    # Smooth and normalize hits map
-    nhits_map = hp.smoothing(
-        hp.ud_grade(nhits_map, nside, power=-2, dtype=np.float64),
-        fwhm=np.pi/180)
-    nhits_map /= np.amax(nhits_map)
-
-    # Get binary mask
-    binary_mask = get_binary_mask_from_nhits(nhits_map, nside, zero_threshold)
-
-    # Multiply by Galactic mask
-    if galactic_mask is not None:
-        binary_mask *= hp.ud_grade(galactic_mask, nside)
-
-    # Apodize the binary mask
-    binary_mask = nmt.mask_apodization(binary_mask, apod_radius,
-                                       apotype=apod_type)
-
-    # Multiply with point source mask
-    if point_source_mask is not None:
-        binary_mask *= hp.ud_grade(point_source_mask, nside)
-        binary_mask = nmt.mask_apodization(binary_mask,
-                                           apod_radius_point_source,
-                                           apotype=apod_type)
-
-    return nhits_map * binary_mask
-
-
-def get_spin_derivatives(map):
-    """
-    First and second spin derivatives of a given spin-0 map.
-    """
-    nside = hp.npix2nside(np.shape(map)[-1])
-    ell = np.arange(3*nside)
-    alpha1i = np.sqrt(ell*(ell + 1.))
-    alpha2i = np.sqrt((ell - 1.)*ell*(ell + 1.)*(ell + 2.))
-    first = hp.alm2map(hp.almxfl(hp.map2alm(map), alpha1i), nside=nside)
-    second = hp.alm2map(hp.almxfl(hp.map2alm(map), alpha2i), nside=nside)
-    cmap = cm.YlOrRd
-    cmap.set_under("w")
-
-    return first, second
 
 
 def read_beam_from_file(beam_file, lmax=None):