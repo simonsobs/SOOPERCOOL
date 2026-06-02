97ebecfad04263e102e838e024112f22f4d24a36
Adrien La Posta <adrien.la.posta@gmail.com>
2026-06-02 14:43:14 +0100

    beam support

--- pipeline/compute_pseudo_cells.py
+++ pipeline/compute_pseudo_cells.py
@@ -3,6 +3,7 @@ from soopercool import ps_utils as pu
 from soopercool import map_utils as mu
 from soopercool import fft_utils as sfft
 from soopercool import coupling_utils as cu
+import soopercool.utils as su
 import argparse
 import numpy as np
 import pymaster as nmt
@@ -148,6 +149,23 @@ def main(args):
     )
     inv_couplings_beamed = {}
     for ms1, ms2 in meta.get_ps_names_list(type="all", coadd=True):
+
+        _, bl1 = su.read_beam_from_file(
+            "/".join(
+                meta.beam_dir_from_map_set(ms1),
+                meta.beam_file_from_map_set(ms1)
+            ),
+            lmax=meta.lmax
+        )
+        _, bl2 = su.read_beam_from_file(
+            "/".join(
+                meta.beam_dir_from_map_set(ms2),
+                meta.beam_file_from_map_set(ms2)
+            ),
+            lmax=meta.lmax
+        )
+        beam = np.outer(bl1, bl2)
+
         transfer = cu.load_transfer_function(
             meta.transfer_settings["transfer_directory"],
             ms1, ms2,
@@ -160,7 +178,7 @@ def main(args):
             nmt_bins,
             transfer=transfer,
             compute_Dl=meta.compute_Dl,
-            beam=None
+            beam=beam
         )
         inv_couplings = inv_couplings.reshape([n_bins*9, n_bins*9])
         inv_couplings_beamed[ms1, ms2] = inv_couplings
